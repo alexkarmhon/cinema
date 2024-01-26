0ba4a2da11a17e7c7e9889a5f1d58996c935047d
alexkarmhon <89stigmat89@gmail.com>
2024-01-26 12:14:09 +0200

    ..+

---
+++ src/js/theme-switcher.js
@@ -0,0 +1,31 @@
+const switcher = document.querySelector('#theme-btn');
+const body = document.querySelector('body');
+const iconPath = document.querySelector('#theme-icon-path');
+
+const { LIGHT, DARK } = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const sun = './sprite.5ec50489.svg#icon-sun';
+const moon = './sprite.5ec50489.svg#icon-moon'
+
+let pageTheme = localStorage.getItem('page-theme') || LIGHT;
+body.classList.add(pageTheme);
+iconPath.attributes.href.textContent = pageTheme === LIGHT ? sun : moon;
+
+
+
+function handlerSwitcher() {
+  body.classList.toggle(LIGHT);
+  body.classList.toggle(DARK);
+  
+  switcher.classList.toggle(LIGHT);
+  switcher.classList.toggle(DARK);
+  
+  pageTheme = pageTheme === LIGHT ? DARK : LIGHT;
+  localStorage.setItem('page-theme', pageTheme);
+  iconPath.attributes.href.textContent = pageTheme === LIGHT ? sun : moon;
+}
+
+switcher.addEventListener('click', handlerSwitcher);
